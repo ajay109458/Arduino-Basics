7d496fa81aadb8af7dbb98053c688fd8a30379d1
Ajay Kumar Yadav <ajay.yadav109458@gmail.com>
2017-12-24 21:34:30 +0530

    Create VibrationSensor.ino

---
+++ ide_src/VibrationSensor.ino
@@ -0,0 +1,30 @@
+  
+const int buzzer = 8; //Buzzer connected to pin 8 of Arduino uno / mega
+int sensor;           //Variable to store analog value (0-1023)
+
+
+void setup()
+{
+  Serial.begin(9600);      //Only for debugging
+  pinMode(buzzer, OUTPUT);
+}
+
+void loop()
+{
+  sensor = analogRead(A0);
+  //While sensor is not moving, analog pin receive 1023~1024 value
+  if (sensor<1022){
+    tone(buzzer, 500);
+    Serial.print("Sensor Value: ");
+    Serial.println(sensor);
+  }
+  
+  else{ 
+    noTone(buzzer);
+    Serial.print("Sensor Value: ");
+    Serial.println(sensor);
+  }
+  
+  
+  delay(100); //Small delay
+}
